e5fef6864399b3521b80f2807c008b3bb8c8a4b6
Volkov Maksim <volkovmaxim02@mail.ru>
2026-07-16 20:30:04 +0300

    Update task1 and task2

--- task1/src/task1.java
+++ task1/src/task1.java
@@ -21,38 +21,48 @@ public class task1 {
             return;
         }
 
-        int n1 = Integer.parseInt(args[0]);
-        int m1 = Integer.parseInt(args[1]);
-        int n2 = Integer.parseInt(args[2]);
-        int m2 = Integer.parseInt(args[3]);
-
-        int[] arr1 = new int[n1];
-        int[] arr2 = new int[n2];
-
-        for(int i = 0; i < arr1.length; i++) {
-            arr1[i] = i + 1;
-        }
-
-        for(int i = 0; i < arr2.length; i++) {
-            arr2[i] = i + 1;
-        }
-
-        String[] path = new String[2];
-
-        Thread t1 = new Thread(() -> { path[0] = buidPath(arr1, m1);});
-        Thread t2 = new Thread(() -> { path[1] = buidPath(arr2, m2);});
-
-        t1.start();
-        t2.start();
-
         try {
-            t1.join();
-            t2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            int n1 = Integer.parseInt(args[0]);
+            int m1 = Integer.parseInt(args[1]);
+            int n2 = Integer.parseInt(args[2]);
+            int m2 = Integer.parseInt(args[3]);
+
+            if (n1 <= 0 || m1 <= 0 || n2 <= 0 || m2 <= 0) {
+                System.out.println("Ошибка: n и m должны быть положительными числами");
+                return;
+            }
+
+            int[] arr1 = new int[n1];
+            int[] arr2 = new int[n2];
+
+            for(int i = 0; i < arr1.length; i++) {
+                arr1[i] = i + 1;
+            }
+
+            for(int i = 0; i < arr2.length; i++) {
+                arr2[i] = i + 1;
+            }
+
+            String[] path = new String[2];
+
+            Thread t1 = new Thread(() -> { path[0] = buidPath(arr1, m1);});
+            Thread t2 = new Thread(() -> { path[1] = buidPath(arr2, m2);});
+
+            t1.start();
+            t2.start();
+
+            try {
+                t1.join();
+                t2.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
+            System.out.println(path[0] + path[1]);
+        } catch (NumberFormatException e) {
+            System.out.println("Ошибка: неверный формат входных данных");
         }
 
-
-        System.out.println(path[0] + path[1]);
     }
 }
--- task2/src/task2.java
+++ task2/src/task2.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class task2 {
     public static void main(String[] args) {
         if (args.length < 2) {
-            System.out.println("Ошибка: нужно 2 аргумента: файл_эллирса файл_точек");
+            System.out.println("Ошибка: нужно 2 аргумента: файл_эллипса файл_точек");
             return;
         }
 
@@ -27,15 +27,46 @@ public class task2 {
 
         try (BufferedReader reader1 = new BufferedReader(new FileReader(args[0]));
             BufferedReader reader2 = new BufferedReader(new FileReader(args[1]))) {
-            String[] center = reader1.readLine().trim().split(" ");
+
+
+            String line;
+
+            String centerLine = null;
+
+            while((line = reader1.readLine()) != null) {
+                if (!line.trim().isEmpty()) {
+                    centerLine = line;
+                    break;
+                }
+            }
+
+            if (centerLine == null) {
+                System.out.println("Ошибка: файл + " + args[0] + " не содержит данных!" );
+                return;
+            }
+            String[] center = centerLine.trim().split(" ");
+
             double x0 = Double.parseDouble(center[0]);
             double y0 = Double.parseDouble(center[1]);
 
-            String[] radius = reader1.readLine().trim().split(" ");
+            String radiusLine = null;
+
+            while((line = reader1.readLine()) != null ) {
+                if(!line.trim().isEmpty()) {
+                    radiusLine = line;
+                    break;
+                }
+            }
+
+            if (radiusLine == null) {
+                System.out.println("Ошибка: файл + " + args[0] + " не содержит строки с радиусом!" );
+                return;
+            }
+
+            String[] radius = radiusLine.trim().split(" ");
             double rx = Double.parseDouble(radius[0]);
             double ry = Double.parseDouble(radius[1]);
 
-            String line;
 
             if (rx == 0 || ry == 0) {
                 System.out.println("Ошибка: радиус не может быть равен 0");
